fc11eb40d6e0d12fad5c6d476c2c6139db9bdeee
DuduOliveira3121 <duduoliveiras3121@gmail.com>
2026-01-21 13:35:50 -0300

    Adicionei a função send na classe netcat

--- NETCAT/netcat.py
+++ NETCAT/netcat.py
@@ -18,6 +18,56 @@ class NetCat:
             self.listen()
         else:
             self.send
+    
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('Interrompido pelo usuário')
+            self.socket.close()
+            sys.exit()
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target = self.handle, args = (client_socket,)
+            )
+            client_thread.start()
+        
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
                                     
 def execute(cmd):
     cmd = cmd.strip()
@@ -31,13 +81,13 @@ if __name__ == '__main__':
         description='Black Hat Python - Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''Exemplo:
-            netcat.py -t 192.168.1.108 -p 5555 -l -c # shell de comando
-            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt #f azer upload do arquivo
-            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # executar comando
+            netcat.py -t 192.168.1.108 -p 5555 -l -c # Shell de comando
+            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # Fazer upload do arquivo
+            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # Executar comando
                                
-            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # enviar texto para a porta 135 do servidor
+            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # Enviar texto para a porta 135 do servidor
             
-            netcat.py -t 192.168.1.108 -p 5555 # conectar o servidor
+            netcat.py -t 192.168.1.108 -p 5555 # Conectar o servidor
         '''))
     parser.add_argument('-c', '--command', action='store_true', help='Shell de comando')
     parser.add_argument('-e', '--execute', help='Executar comando especificado')
@@ -53,3 +103,45 @@ if __name__ == '__main__':
 
     nc = NetCat(args, buffer.encode())
     nc.run()
+
+class NetCat:
+    def __init__(self, args, buffer=None)
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+    
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+
+        try:
+            while true:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.encode()
+
+                    if recv_len < 4096:
+                        break
+                
+                if response:
+                    print(response)
+                    buffer = input('> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        
+        except KeyboardInterrupt:
+            print('Interrompido pelo usuário')
+            self.socket.close()
+            sys.exit()
+        
