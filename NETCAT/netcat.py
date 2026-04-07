d63a3f56235211788ebd126739e6fdec52559bb1
DuduOliveira3121 <duduoliveiras3121@gmail.com>
2026-04-07 00:12:14 -0400

    fix: Editing execute and handle functions

--- NETCAT/netcat.py
+++ NETCAT/netcat.py
@@ -11,7 +11,7 @@ def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex, stderr=subprocess.STDOUT, shell=True)
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
     return output.decode()
 
